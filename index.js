508d8153521ab8563e6a1574de7f5e233eb6fe18
niawjunior <niawjunior@niawjuniors-MacBook-Pro.local>
2020-10-04 19:53:55 +0700

    update code

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(ExpressAPILogMiddleware(logger, { request: true }));
 
 app.get('/get', (req, res) => {
-    res.status(200).send('hello world');
+    res.status(200).send('hello niaw');
 });
 
 app.listen(config.port, config.host, (e)=> {
